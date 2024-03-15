5c7ed9ffb3a229bb8bf8fb0e36d298b9f4bc7c85
Nikoline Mai Bogely Rehn <nikoline@rehn.dk>
2024-03-15 16:40:08 +0000

    gracy

---
+++ project/cpu/gracy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#BSUB -J cpu # name
+#BSUB -o outfiles/close_%J.out # output file
+#BSUB -q hpcintrogpush 
+#BSUB -n 72 ## cores
+#BSUB -R "rusage[mem=1GB]" 
+#BSUB -W 60 # useable time in minutes
+##BSUB -N # send mail when done
+#BSUB -R "span[hosts=1]"
+
+N=100 ## 55, 165, 275, 385
+ITER=1000 ## 200000, 8000, 1500, 600
+TOLERANCE=-1
+START_T=5
+
+lscpu
+
+FOLDER="../results/cpu/gracy"
+FILE_REDUCTION=$FOLDER/reduction.txt
+FILE_NO_REDUCTION=$FOLDER/no_reduction.txt
+
+rm -rf $FILE_REDUCTION
+rm -rf $FILE_NO_REDUCTION
+
+for threads in {1..72..1};
+do  
+    echo -n $threads " " >> $FILE_REDUCTION
+    echo -n $threads " " >> $FILE_NO_REDUCTION
+    OMP_NUM_THREADS=$threads OMP_SCHEDULE=static OMP_PROC_BIND=close OMP_PLACES=cores ./jacobi_reduction $N $ITER $TOLERANCE $START_T >> $FILE_REDUCTION
+    OMP_NUM_THREADS=$threads OMP_SCHEDULE=static OMP_PROC_BIND=close OMP_PLACES=cores ./jacobi_no_reduction $N $ITER $TOLERANCE $START_T >> $FILE_NO_REDUCTION
+
+done
+
+exit 0
+
